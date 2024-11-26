cdc70c0d3200fed60e66f55642e634a5c17da8ec
KissAkos <kopyherceg@gmail.com>
2024-11-26 21:13:24 +0200

    updated scrape.py

--- web_scraper/scrape.py
+++ web_scraper/scrape.py
@@ -2,4 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
+links = soup.select('.titleline')
+votes = soup.select('.score')
+
+print(links[0])
+print(votes[0])
