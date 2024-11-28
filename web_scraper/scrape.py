84621cf7846cf300366dac7516dc62ca1cfae356
KissAkos <kopyherceg@gmail.com>
2024-11-28 23:20:37 +0200

    created selectors for a the needed data from the web page

--- web_scraper/scrape.py
+++ web_scraper/scrape.py
@@ -8,5 +8,13 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline')
 votes = soup.select('.score')
 
-print(links[0])
-print(votes[0])
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].select('a')[0]['href']
+        points = int(votes[idx].getText().replace(' points', ''))
+        hn.append({'title': title, 'link': href})
+    return hn
+
+print(create_custom_hn(links, votes))
