51433d255b90ed95cdbe2e30d4fb6f440e833b3a
KissAkos <kopyherceg@gmail.com>
2024-11-30 17:10:18 +0200

    added sorting for the stories by vote

--- web_scraper/scrape.py
+++ web_scraper/scrape.py
@@ -1,20 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 
 soup = BeautifulSoup(res.text, 'html.parser')
 
 links = soup.select('.titleline')
-votes = soup.select('.score')
+subline = soup.select('.subline')
 
-def create_custom_hn(links, votes):
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
+
+def create_custom_hn(links, subline):
     hn = []
-    for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].select('a')[0]['href']
-        points = int(votes[idx].getText().replace(' points', ''))
-        hn.append({'title': title, 'link': href})
-    return hn
+    for idx, item in enumerate(links): 
+        title = item.getText()
+        href = item.select('a')[0]['href']
+        vote = subline[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
-print(create_custom_hn(links, votes))
+pprint.pprint(create_custom_hn(links, subline))
